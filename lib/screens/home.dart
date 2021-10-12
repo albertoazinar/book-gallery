a1499cd6b8551a544b08a0f9055754e5504236cf
AlbertoYabeda <albertoacmacuacua@gmail.com>
2021-10-12 02:01:18 +0100

    book card ready to receive data

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -113,8 +113,133 @@ class _HomeState extends State<Home> {
                 ),
 
                 SizedBox(
-                  height: 20,
+                  height: 25,
                 ),
+
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 26),
+                  child: Container(
+                    height: 160,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: [
+                       BoxShadow(
+                         color: Colors.grey.shade300,
+                         blurRadius: 6,
+                         spreadRadius: 2
+                       )
+                      ]
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(0),
+                      child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(26)
+                            ),
+                            child: Row(
+                                children: [
+                                    Padding(
+                                      padding: const EdgeInsets.all(6),
+                                      child: ClipRRect(
+                                        borderRadius: BorderRadius.circular(26),
+                                        child: SizedBox(
+                                          width: 105,
+                                          height: 300,
+                                          child: Image.network(
+                                              "https://books.google.com/books?id=iwiYGwAACAAJ&printsec=frontcover&img=1&zoom=5&sig=_L6ySKDAs-8gNK28c3NyFdO22ZM",                                               fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+
+                                    Expanded(
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(left: 15),
+                                                child: Column(
+                                                  children: [
+                                                    SizedBox(
+                                                      height: 26,
+                                                    ),
+                                                    Text(
+                                                      "by Joshua Becker",
+                                                      style: TextStyle(
+                                                        fontSize: 15,
+                                                        color: Colors.grey,
+                                                        fontFamily: 'Roboto',
+                                                      ),
+                                                      textAlign: TextAlign.left,
+                                                    ),
+                                                    SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                    Text(
+                                                      "The More of Less",
+                                                        style: TextStyle(
+                                                            fontSize: 18,
+                                                            color: Colors.black,
+                                                            fontWeight: FontWeight.w700
+                                                        ),
+                                                      textAlign: TextAlign.left,
+                                                      ),
+                                                     Row(
+                                                        mainAxisAlignment: MainAxisAlignment.start,
+                                                        crossAxisAlignment: CrossAxisAlignment.end,
+                                                        children: [
+                                                            Icon(
+                                                                Icons.star_rate_rounded,
+                                                                color: Colors.yellow,
+                                                            ),
+                                                            SizedBox(width: 6),
+                                                            Text(
+                                                                "4.5",
+                                                                style: TextStyle(
+                                                                  fontSize: 15,
+                                                                  color: Colors.grey
+                                                                ),
+                                                              textAlign: TextAlign.left,
+                                                            )
+                                                      ],
+                                                    ),
+                                                    SizedBox(
+                                                      height: 10,
+                                                    ),
+
+                                                    Container(
+                                                      decoration: BoxDecoration(
+                                                        borderRadius: BorderRadius.circular(15),
+                                                        color: Colors.blue.shade200
+                                                      ),
+                                                      child: Padding(
+                                                        padding: const EdgeInsets.symmetric(horizontal: 12),
+                                                        child: Padding(
+                                                            padding: const EdgeInsets.symmetric(vertical: 4),
+                                                          child: Text(
+                                                              "Minimalism",
+                                                            style: TextStyle(
+                                                                fontSize: 15,
+                                                                color: Colors.blue.shade700
+                                                            ),
+                                                            textAlign: TextAlign.center,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    )
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                        )
+                                    )
+                                ],
+                            ),
+                      ),
+                    )
+                  ),
+                )
               ],
             )
 
